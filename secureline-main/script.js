07344f63b0e058071276a6083e0a3aba85b269d9
hehecau <161074322+hehecau@users.noreply.github.com>
2026-06-12 20:54:36 +0200

    pridane zlavy 20%

---
+++ secureline-main/script.js
@@ -0,0 +1,37 @@
+const rewardLabel = document.getElementById("rewardLabel");
+
+const zelezoLabel = document.getElementById("zelezoInput");
+zelezoLabel.addEventListener("input", updateReward);
+
+const zlatoLabel = document.getElementById("zlatoInput");
+zlatoLabel.addEventListener("input", updateReward);
+
+const cinLabel = document.getElementById("cinInput");
+cinLabel.addEventListener("input", updateReward);
+
+const hlinikLabel = document.getElementById("hlinikInput");
+hlinikLabel.addEventListener("input", updateReward);
+
+const medLabel = document.getElementById("medInput");
+medLabel.addEventListener("input", updateReward);
+
+const zinokLabel = document.getElementById("zinokInput");
+zinokLabel.addEventListener("input", updateReward);
+
+const ceny = {zelezo: 4500, zlato: 37000, cin: 11000, hlinik: 2000, med: 3000, zinok: 3000};
+
+function updateReward() {
+    const reward =
+        (Number(zelezoInput.value) || 0) * ceny.zelezo +
+        (Number(zlatoInput.value) || 0) * ceny.zlato +
+        (Number(cinInput.value) || 0) * ceny.cin +
+        (Number(hlinikInput.value) || 0) * ceny.hlinik +
+        (Number(medInput.value) || 0) * ceny.med +
+        (Number(zinokInput.value) || 0) * ceny.zinok;
+
+    const formatted = new Intl.NumberFormat('sk-SK').format(reward);
+
+    rewardLabel.textContent = `${formatted} Kč`;
+}
+
+updateReward();
